1f127a850a13ca541b5bcfc429cb86820f7574f2
Takeru Yamanaka <Takeru@takeru-no-MacBook-Pro-Retina.local>
2019-09-21 19:50:11 +0900

    [facebook認証実装完了]insta

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,13 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-# class OmniauthCallbacksController < ApplicationController
-    # < ApplicationController
+    # メンターさんより
+    # gitにomniauthジェムによるSNS認証の方法がしっかりと記述されている
+    # コントローラの名前の書き方は、classの継承が行われている。
+    # controllers内のディレクトリ::コントローラ名　<右のコントローラの内容を継承します< Deviseコントローラは見えてないけど存在する::OmniauthCallbacksControllerもその一つ
+    # 左のコントローラ内にアクションを記述していくことで、右のコントローラへと書き足しているのと同義になる。
+    # Deviseのコントローラは継承という形で視える化することが可能。継承なので機能の上書きや、書き足しなどのカスタマイズもできるが、ここのユーザのアクションを変更してしまう可能性があるためセキュリティ的に微妙かも…
+
+    # class OmniauthCallbacksController < ApplicationController
+    # 上記の記述ではアプリケーションコントローラを継承しているため、deviseのコントローラとしての機能を継承できていない
     def facebook
         callback_from :facebook
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
         #  omuniauthtableを追加する
+        #  :omniauthtableをONにする
 
          validates :name, presence: true, length: {minimum: 1, maximum: 20}
          validates :profile_name, presence: true, length: {minimum: 1, maximum: 100}
+        # facebook認証で必要な項目、以外でvalidatesを掛けていると（今回の場合はprofile_name）createメソッドが実行される際に、必要なカラムが埋まらず（今回はpresence: trueをしていたため）rollbackされてしまう。
+        # カラムに必要な情報が渡っているか、validatesが渡す情報の邪魔をしていないか要チェック
+
+        # nameとprofile_nameにauth.info.nameの情報を渡してみてはいかが？
 
   has_many :post_images, dependent:  :destroy
   has_many :post_comments, dependent:  :destroy
@@ -16,15 +21,24 @@ class User < ApplicationRecord
 
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
+    # 最初にuidの存在をUserモデルから探しに活かせる
+    # 探すのはuidとproviderで、その条件にヒットするユーザかどうかを条件分岐
+    # facebook側による認証がfacebookページで最初に行われる
+    # facebook側から受け取ったuid、providerを元にUserモデルにwhereメソッドを実行する。
 
     unless user
       user = User.create(
         uid:      auth.uid,
         provider: auth.provider,
+        name:     auth.info.name,
+        profile_name:  auth.info.name,
         email:    auth.info.email,
         password: Devise.friendly_token[0,20]
       )
     end
+    # 変数userに代入できるuidがない場合は、createメソッドをuserに代入する
+    # Userモデル内のカラムを指定:  =>  facebookから渡されるinfoを記述（参考はgithub）
+    # passwordは0 - 20 桁で自動生成させることで完了。
 
     user
   end
